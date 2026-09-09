cccbc934687585e86e17e22c3229339d1b8f0c58
Татаринов Данил <daniltatarinov@inbox.ru>
2026-09-10 00:54:56 +0500

    Task3

---
+++ Tyuiu.TatarinovDA.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.TatarinovDA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.TatarinovDA.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
