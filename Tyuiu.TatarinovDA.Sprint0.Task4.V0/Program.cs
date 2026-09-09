3ad5385eadb4c4d4f0479665d645a0836d3f8778
Татаринов Данил <daniltatarinov@inbox.ru>
2026-09-10 01:15:46 +0500

    Task4

---
+++ Tyuiu.TatarinovDA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.TatarinovDA.Sprint0.Task4.V0.Lib;
+Console.WriteLine(DataService.Addition(1,5));
+
+Console.WriteLine(DataService.Substraction(15, 5));
+
+Console.WriteLine(DataService.Multiplication(10, 10));
+
+Console.WriteLine(DataService.Division(5, 5));
+
+Console.ReadKey();
