b6aa7a10a9d1f5e23ffc0e18196e8daf208d3671
Татаринов Данил <daniltatarinov@inbox.ru>
2026-09-10 00:37:10 +0500

    Task2

---
+++ Tyuiu.TatarinovDA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.TatarinovDA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.TatarinovDA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Данил";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет..., Данил", res);
+        }
+    }
+}
---
+++ Tyuiu.TatarinovDA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.TatarinovDA.Sprint0.Task2.V0.Lib;
+Console.WriteLine(DataService.GetMessage("Данил"));
+Console.ReadKey();
+
